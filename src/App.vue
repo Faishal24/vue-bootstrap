0c67df9e8a88e5b0991c030acd8a0651247d9874
Faishal <ishal.2401@gmail.com>
2024-11-24 19:49:08 +0700

    Add cards

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <div>
     <Navbar />
     <Hero />
+    <Cards />
   </div>
 </template>
 
 <script>
+import Cards from "./components/Cards.vue";
 import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
 
 export default {
   components: {
     Navbar,
-    Hero
+    Hero,
+    Cards
   },
 };
 </script>
-<style></style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mt-5 mx-5">
+    <div class="row justify-content-center">
+      <div
+        v-for="(image, index) in images"
+        class="col-4"
+        style="width: 18rem"
+      >
+        <div :key="index"
+        class="card">
+          <img :src="image.download_url" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+export default {
+  setup() {
+    const images = ref([]);
+
+    onMounted(() => {
+      axios
+        .get("https://picsum.photos/v2/list?page=12")
+        .then((response) => (images.value = response.data.slice(0, 10)))
+        .catch((error) => console.log(error));
+    });
+
+    return { images };
+  },
+};
+</script>
